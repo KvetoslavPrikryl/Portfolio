461b08ef8be3c2c1ea73e76d822bb3c25cb1ec50
KvetoslavPrikryl <kveta.prikryl1991@gmail.com>
2022-07-12 15:18:03 +0200

    create HeroImg.js and HeroImgStyles.css and in HeroImg.js add img and two links

---
+++ src/components/HeroImg.js
@@ -0,0 +1,23 @@
+import "./HeroImgStyles.css";
+import React from 'react';
+import IntroImg from "../Img/Home_img.jpg"
+import {Link} from "react-router-dom";
+
+const HeroIMG = () => {
+  return (
+    <div className="hero">
+        <div className="mask">
+            <img className="into-img" src={IntroImg} alt="IntroImg"/>
+        </div>
+        <div className="content">
+            <h1>Fullstek Developer</h1>
+            <div>
+                <Link to="/project" className="btn">Projects</Link>
+                <Link to="/contact" className="btn">Contact</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroIMG
