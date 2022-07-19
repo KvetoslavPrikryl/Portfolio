bd87833c8baef0c7ba2a63218da4153f3c81f52d
KvetoslavPrikryl <kveta.prikryl1991@gmail.com>
2022-07-19 14:52:17 +0200

    styling HeroImg2

--- src/components/HeroImg2.js
+++ src/components/HeroImg2.js
@@ -13,8 +13,7 @@ const HeroImg2 = () => {
             <div className="card">
                 <img className="hero_img" src={Hero_img} alt="Hero_img"/>
                 <p className="hero_text">
-                    Jmenuji se Květoslav Přikryl. Programování se věnuji cca 2 roky převážně pogramování v Pythonu. Chodil jsem 8 měsíců do ItStep academie na kurz programování v pythonu.
-                    
+                    Jmenuji se Květoslav Přikryl. Programování se věnuji cca 2 roky, převážně pogramování v Pythonu. Chodil jsem 8 měsíců do ItStep academie na kurz programování v pythonu. Nejdříve jsem se začal učit HTML a CSS, pak jsem se zaměřil na Python, kde programuji jednoduché aplikace. Pokud by se pro stejný účel použil smysluplný text, bylo by těžké hodnotit pouze vzhled, aniž by se pozorovatel nechal svést ke čtení obsahu. Pokud by byl naopak použit nesmyslný, ale pravidelný text (např. opakování „asdf asdf asdf…“), oko by při posuzování vzhledu bylo vyrušováno pravidelnou strukturou textu, která se od běžného textu liší. Text lorem ipsum na první pohled připomíná běžný text, slova jsou různě dlouhá, frekvence písmen je podobná běžné řeči, interpunkce vypadá přirozeně atd.
                 </p>
             </div>
             
